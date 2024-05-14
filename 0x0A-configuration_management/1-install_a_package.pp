e9f248d9c4fe9f274fc7b924928e88f4b58612e4
reeperx <acebany080@gmail.com>
2024-05-14 03:56:24 -0700

    new

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,9 +3,3 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-
-exec { 'upgrade_werkzeug':
-  command => 'pip3 install --upgrade Werkzeug==2.1.1',
-  path    => '/usr/local/bin',
-  require => Package['flask'],
-}
