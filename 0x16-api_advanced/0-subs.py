7662f56289ab23c9aa6f24a3cd0407222890b4cc
reeperx <acebany080@gmail.com>
2024-08-13 16:17:50 -0700

    changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-'''a function that queries the Reddit API and returns the number of subscribers for a given subreddit.'''
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    '''Returns total number of subscribers on a given subreddit.'''
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-            }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            return 0
-
-        data = response.json().get("data", {})
-        return data.get("subscribers", {})
-    except requests.RequestException:
-        return 0
-    except ValueError:
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
